f3692dea439cd7def5d0356655e41b64bbf903f9
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-03-01 12:37:52 +0200

    Added questions route to get 10 random questions

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  rules: {
-  },
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['airbnb-base'],
+    parserOptions: {
+        ecmaVersion: 'latest',
+    },
+    rules: {
+        indent: ['error', 4],
+    },
 };
