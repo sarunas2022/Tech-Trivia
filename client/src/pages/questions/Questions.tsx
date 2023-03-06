7a0364e324dca4eb686b70995986cbdf21086c91
Šarūnas Grabauskas <sarunas.grabauskas@gmail.com>
2023-03-06 16:34:10 +0200

    added functionality to fetch questions

--- client/src/pages/questions/Questions.tsx
+++ client/src/pages/questions/Questions.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+interface AnswersArray {
+    id: number;
+    answer: string;
+    correct: number;
+}
+interface QuestionsArray {
+    id: number;
+    topic: string;
+    question: string;
+    answers: AnswersArray[];
+}
 
 const Questions: React.FC = () => {
-    return <div>questions</div>;
+    const [questions, setQuestions] = useState<QuestionsArray[]>([]);
+    console.log(questions);
+
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            try {
+                const response: Response = await fetch(
+                    'http://localhost:8080/questions?topic=All'
+                );
+                const data: QuestionsArray[] = await response.json();
+                setQuestions(data);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchQuestions();
+    }, [questions]);
+
+    return <div></div>;
 };
 
 export default Questions;
