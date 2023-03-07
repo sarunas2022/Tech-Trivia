dd2f51f0ee79d00ae579d435fa4257fe751cfef4
ksmagu <k.smagurauskaite@gmail.com>
2023-03-07 12:40:27 +0200

    added styles

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -18,26 +18,28 @@ const Home: React.FC = () => {
                     <h1>TECH TRIVIA</h1>
                 </div>
                 <img className='monster' src={Monster} alt='img' />
+                </div>
+                <div className='bottomHalf'>
                 <h1 className='start'>Let's get started! Choose a topic:</h1>
                 <div className='topics'>
                     
-                    <Button color='#e36414' onClick={handleClick}>
+                    <Button color='$orange' onClick={handleClick}>
                         HTML
                     </Button>
-                    <Button color='#e36414' onClick={handleClick}>
+                    <Button color='$orange' onClick={handleClick}>
                         CSS
                     </Button>
-                    <Button color='#e36414' onClick={handleClick}>
+                    <Button color='$orange' onClick={handleClick}>
                         React
                     </Button>
-                    <Button color='#e36414' onClick={handleClick}>
+                    <Button color='$orange' onClick={handleClick}>
                         JavaScript
                     </Button>
-                    <Button color='#e36414' onClick={handleClick}>
+                    <Button color='$orange' onClick={handleClick}>
                         I know it all
                     </Button>
                 </div>
-            </div>
+                </div>
         </>
     );
 };
